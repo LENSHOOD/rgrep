b426ebcc41a33f5ed39fc6faa9b735a8bf5d7257
zhangxuhai <lenshood.zxh@gmail.com>
2022-10-19 21:13:17 +0800

    feat: support regex pattern

--- src/matcher/file_matcher.rs
+++ src/matcher/file_matcher.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use async_trait::async_trait;
+use regex::Regex;
 use tokio::fs::File;
 use tokio::io::{AsyncBufReadExt, BufReader};
 
@@ -18,13 +19,18 @@ impl LineMatcher for TextFileLineMatcher {
 
         let mut res = Vec::<String>::new();
         while let Some(line) = lines.next_line().await? {
-            if line.contains(pattern.as_str()) {
+            if is_match_regex(pattern.as_str(), line.as_str())? {
                 res.push(line);
             }
         }
 
         Ok(res)
     }
+}
+
+fn is_match_regex(pattern: &str, to_be_matched: &str) -> Result<bool> {
+    let regex = Regex::new(pattern)?;
+    Ok(regex.is_match(to_be_matched))
 }
 
 impl TextFileLineMatcher {
@@ -43,7 +49,10 @@ mod tests {
     use super::*;
 
     const TEXT_CONTENT: &str = r#"Hello World
+Numbers: 1, 2, 3.456
 File Line Matcher
+多语言
+Match Brackets: [in_the_bracket1233456]
 Text End"#;
 
     #[tokio::test]
@@ -79,6 +88,27 @@ Text End"#;
         // then
         assert_eq!(vec.len(), 1);
         assert_eq!(vec[0], "File Line Matcher");
+        tokio::fs::remove_file(path).await.unwrap()
+    }
+
+    #[tokio::test]
+    async fn should_match_line_with_regex() {
+        // given a new text file
+        let path = "should_match_line_with_regex";
+        let txt = std::fs::File::create(path).unwrap();
+        BufWriter::new(txt)
+            .write_all(TEXT_CONTENT.as_bytes())
+            .unwrap();
+
+        // when
+        let matcher = TextFileLineMatcher::new(path).await.unwrap();
+        let pattern = r"([\d]+)|([\[].+[\]])";
+        let vec = matcher.match_line(pattern.into()).await.unwrap();
+
+        // then
+        assert_eq!(vec.len(), 2);
+        assert_eq!(vec[0], "Numbers: 1, 2, 3.456");
+        assert_eq!(vec[1], "Match Brackets: [in_the_bracket1233456]");
         tokio::fs::remove_file(path).await.unwrap()
     }
 }
